f5ce1d0d61e2432a173e8d123eb021032108a0e4
Rxgerp <rogerosoriou@gmail.com>
2026-03-15 00:05:39 -0500

    TEST1

--- script.js
+++ script.js
@@ -7,6 +7,38 @@ document.getElementById("musica").play();
 }
 
 
+/* CUENTA REGRESIVA */
+
+const fechaObjetivo = new Date("2026-03-25 00:40:00").getTime();
+
+const contador = document.getElementById("contador");
+
+const intervalo = setInterval(()=>{
+
+const ahora = new Date().getTime();
+
+const diferencia = fechaObjetivo - ahora;
+
+if(diferencia <= 0){
+
+clearInterval(intervalo);
+
+contador.innerHTML = "💖 PORFIN JUNTITOS NUEVAMENTE, DAME UN BESITO PORFIS, TE JURO QUE LA PASAREMOS MUY BIEN, TE AMO Y DISFRUTA DE TU VIAJE CONMIGO! 💖";
+contador.classList.add("contador-final");
+
+return;
+
+}
+
+const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+const horas = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
+const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
+
+contador.innerHTML =
+`Joder faltan (${dias} días ${horas}h ${minutos}m ${segundos}s) Para verte!`;
+
+},1000);
 
 /* CORAZONES FLOTANDO CONSTANTES */
 
